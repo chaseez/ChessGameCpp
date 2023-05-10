bca6b4947b7cf230610ca313d8fc93df86291b2b
chaseez <chasezundel@gmail.com>
2023-05-10 16:37:01 -0600

    Added white/black specific input validation

--- main.cpp
+++ main.cpp
@@ -10,18 +10,9 @@ std::string promptUser() {
     return coordinates;
 }
 
-void validateInput(std::string& coord) {
-    bool invalid = true;
-    if(coord.length() != 2) invalid = false;
-
-    while(!invalid) {
-        std::cout << "\nInvalid input" << std::endl;
-        std::cout << "Please enter in the coordinate format like: 'a4'" << std::endl;
-        coord = promptUser();
-        if(coord.length() != 2) continue;
-
-        invalid = true;
-    }
+void validateInput(std::string& coord, const Board& board, bool turn) {
+    bool valid = true;
+    if(coord.length() != 2) valid = false;
 
     // Making the ascii numbers correspond with a column
     int letter = std::tolower(coord[0]) - 'a';
@@ -30,10 +21,10 @@ void validateInput(std::string& coord) {
     std::string temp = coord.substr(1);
     int number = std::stoi(temp) - 1;
 
-    if(letter < 0 or letter >= 8) invalid = false;
-    if(number < 0 or number >= 8) invalid = false;
+    if(letter < 0 or letter >= 8) valid = false;
+    if(number < 0 or number >= 8) valid = false;
 
-    while(!invalid) {
+    while(!valid) {
         std::cout << "\nInvalid input" << std::endl;
         std::cout << "Please choose a coordinate on the board between 'a'-'f' and 1-8" << std::endl;
         coord = promptUser();
@@ -49,7 +40,52 @@ void validateInput(std::string& coord) {
         if(letter < 0 or letter >= 8) continue;
         if(number < 0 or number >= 8) continue;
 
-        invalid = true;
+        valid = true;
+    }
+
+    int pieceType = board.getBoard()[number][letter];
+
+    // true == White's turn
+    // false == Black's turn
+    if(turn) {
+        if(pieceType == 1 or pieceType == 3 or pieceType == 5 or pieceType == 7 or pieceType == 9 or pieceType == -2); // White's piece values
+        else valid = false;
+    }
+    else {
+        if(pieceType == 2 or pieceType == 4 or pieceType == 6 or pieceType == 8 or pieceType == 10 or pieceType == -1); // Black's piece values
+        else valid = false;
+    }
+
+    if(!valid) {
+        while(!valid) {
+            std::cout << "\nInvalid input" << std::endl;
+            std::cout << "That's not your piece" << std::endl;
+            coord = promptUser();
+            if(coord.length() != 2) continue;
+
+            // Making the ascii numbers correspond with a column
+            letter = std::tolower(coord[0]) - 'a';
+
+            // Adjusting the number to fit the array index
+            temp = coord.substr(1);
+            number = std::stoi(temp) - 1;
+
+            if(letter < 0 or letter >= 8) continue;
+            if(number < 0 or number >= 8) continue;
+
+            pieceType = board.getBoard()[number][letter];
+
+            // true == White's turn
+            // false == Black's turn
+            if(turn) {
+                if(pieceType == 1 or pieceType == 3 or pieceType == 5 or pieceType == 7 or pieceType == 9 or pieceType == -2) valid = true;
+                else continue;
+            }
+            else {
+                if(pieceType == 2 or pieceType == 4 or pieceType == 6 or pieceType == 8 or pieceType == 10 or pieceType == -1) valid = true;
+                else continue;
+            }
+        }
     }
 
 }
@@ -66,17 +102,19 @@ int main() {
     bool winning = false;
     bool turn = true;
 
+    // true == White's turn
+    // false == Black's turn
     while(!winning)
     {
         std::string coordinates;
         coordinates = promptUser();
 
-        validateInput(coordinates);
+        validateInput(coordinates, board, turn);
 
 
+        if(!turn) winning = true;
 
         turn = !turn;
-        winning = true;
     }
 
     return 0;
